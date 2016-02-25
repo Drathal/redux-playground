5f784185f6e5380cf6b880387db3b459a9a5ff29
Markus Dethlefsen <dethlefsen@cleverbridge.com>
2016-02-25 16:41:03 +0100

    remove not needed imports

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,7 @@
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
 
 import makeStore from './store'
 import App from '../containers/App'
