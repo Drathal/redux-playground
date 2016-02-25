f6e807b6ddcc9cca74fb3c20739ea5a200d51845
Markus Dethlefsen <dethlefsen@cleverbridge.com>
2016-02-25 09:58:02 +0100

    add some comments to cart actions

--- src/redux/modules/cart/actions.js
+++ src/redux/modules/cart/actions.js
@@ -4,20 +4,38 @@ export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 export const DECREMENT_CARTITEM_QUANTITY = 'DECREMENT_CARTITEM_QUANTITY';
 export const INCREMENT_CARTITEM_QUANTITY = 'INCREMENT_CARTITEM_QUANTITY';
 
-/* action creators */
-
+/**
+ * adds a product to the cart and update the quantity
+ * @param  {number} productId
+ * @return {action}
+ */
 export const addItem = (productId) => {
     return {type: ADD_TO_CART, productId}
 }
 
+/**
+ * delete product completly from cart
+ * @param  {number} productId
+ * @return {action}
+ */
 export const deleteItem = (productId) => {
     return {type: DELETE_FROM_CART, productId}
 }
 
+/**
+ * decrement product quantity from cart
+ * @param  {number} productId
+ * @return {action}
+ */
 export const decrementQuantity = (productId) => {
     return {type: DECREMENT_CARTITEM_QUANTITY, productId}
 }
 
+/**
+ * inccrement product quantity from cart
+ * @param  {number} productId
+ * @return {action}
+ */
 export const incrementQuantity = (productId) => {
     return {type: INCREMENT_CARTITEM_QUANTITY, productId}
 }
