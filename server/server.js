2faf7fc7a9740aa66dacbcdd9ac72b089459442f
Markus Dethlefsen <dethlefsen@cleverbridge.com>
2016-02-28 13:08:05 +0100

    refactor server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
+var path = require('path')
 var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
+var config = require('../webpack.config')
 
 var app = new (require('express'))()
 var port = 3000
@@ -11,13 +12,13 @@ app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.
 app.use(webpackHotMiddleware(compiler))
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(path.join(__dirname, '/../src/app/index.html'))
 })
 
 app.listen(port, function(error) {
     if (error) {
         console.error(error)
     } else {
-        console.info('==> Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port)
+        console.info('==> Open up http://localhost:%s/ in your browser.', port)
     }
 })
