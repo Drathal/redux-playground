ccf7dfd6bb6f95bbb8f4f85d659da9715a3fc458
Markus Dethlefsen <dethlefsen@cleverbridge.com>
2016-02-29 00:36:41 +0100

    add production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 var path = require('path')
 var webpack = require('webpack')
 
+var isProduction = !!process.argv.find(x => x == '-p')
+
+var entry = {app: ['./index', 'webpack-hot-middleware/client']}
+
+if (isProduction) {
+    entry = {app: ['./index'], vendor: ['react', 'react-dom', 'redux', 'redux-promise', 'redux-thunk', 'redux-logger', 'babel-polyfill']}
+}
+
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
-    entry: [
-        'webpack-hot-middleware/client',
-        './index'
-    ],
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    entry,
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'build'),
         filename: 'bundle.js',
         publicPath: '/build/'
     },
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({name: 'vendor', minChunks: Infinity, filename: 'vendor.js'}),
+        new webpack.optimize.UglifyJsPlugin({mangle: true, compress: {warnings: false}})
     ],
     module: {
         loaders: [
