5665de6380a7c6cbad145f04a3cd653d2012339e
Markus Dethlefsen <dethlefsen@cleverbridge.com>
2016-03-31 10:37:16 +0200

    refactor ProductsContainer

--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -4,6 +4,15 @@ import { connect } from 'react-redux'
 import ProductsList from '../components/ProductsList'
 import { addProduct, deleteProduct } from '../redux/modules/products/actions'
 
+const doDeleteProduct = function(product) {
+    this.props.deleteProduct(product.id)
+}
+
+const doAddProduct = function() {
+    let id = Math.floor(Math.random() * (100 - 5) + 5);
+    this.props.addProduct({'id': id, 'description': 'product ' + id})
+}
+
 class ProductsContainer extends Component {
     render() {
 
@@ -12,22 +21,17 @@ class ProductsContainer extends Component {
         return (
             <ProductsList title="My Products">
                 { products.map((product) => <p key={ product.id }>
-                                                { product.id } &nbsp;-&nbsp;
+                                                { product.id }
+                                                <span>-</span>
                                                 { product.description }
-                                                <button onClick={ () => {
-                                                                      this.props.deleteProduct(product.id)
-                                                                  } }>
+                                                <button onClick={ doDeleteProduct.bind(this, product) }>
                                                     x
                                                 </button>
                                             </p>) }
-                <button onClick={ () => {
-                                      let id = Math.floor(Math.random() * (100 - 5) + 5);
-                                      this.props.addProduct({'id': id, 'description': 'product ' + id})
-                                  } }>
+                <button onClick={ doAddProduct.bind(this) }>
                     ADD RANDOM PRODUCT
                 </button>
             </ProductsList>
-
         )
     }
 }
