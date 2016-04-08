f4f3694408937b6da9db210bcb68c388c272a864
Markus Dethlefsen <dethlefsen@cleverbridge.com>
2016-04-08 10:37:57 +0200

    refactor productList tests

--- src/components/views/ProductList/index.spec.js
+++ src/components/views/ProductList/index.spec.js
@@ -12,21 +12,27 @@ describe('component view <ProductList />', () => {
     const onAddProduct = sinon.spy()
     const onDeleteProduct = sinon.spy()
 
-    it('can render ProductList', () => {
-        const wrapper = shallow(<ProductsList products={ products }
-                                              addProduct={ onAddProduct }
-                                              deleteProduct={ onDeleteProduct } />);
+    const wrapper = shallow(<ProductsList products={ products }
+                                          addProduct={ onAddProduct }
+                                          deleteProduct={ onDeleteProduct } />);
 
+    it('can render messages', () => {
         expect(wrapper.html().includes(messages.title)).to.equal(true)
         expect(wrapper.html().includes(messages.addProductButton)).to.equal(true)
         expect(wrapper.html().includes(messages.deleteProductButton)).to.equal(true)
+    });
 
+    it('can render product informations', () => {
         expect(wrapper.html().includes(products[0].description)).to.equal(true)
         expect(wrapper.html().includes(products[0].id)).to.equal(true)
+    });
 
+    it('can handle addProduct', () => {
         wrapper.find('button.addProduct').simulate('click')
         expect(onAddProduct.calledOnce).to.equal(true)
+    });
 
+    it('can handle deleteProduct', () => {
         wrapper.find('button.deleteProduct').simulate('click')
         expect(onDeleteProduct.calledOnce).to.equal(true)
     });
