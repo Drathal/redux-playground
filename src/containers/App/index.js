90645eccd3c682d0ca4c66f948b4b59441b2b6c8
Markus Dethlefsen <privat@markus-inger.de>
2016-04-28 07:46:51 +0200

    remove debug log

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,8 +1,6 @@
 import React, { PropTypes } from 'react'
 import DevTools from '../../containers/DevTools';
 
-console.log(process.env.NODE_ENV)
-
 const App = ({children}) => <div className={ 'app' }>
                                 { children }
                                 { process.env.NODE_ENV === 'development' && <DevTools /> }
