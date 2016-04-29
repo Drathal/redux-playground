9133174478284646f960378924ee5e465122c9d7
Markus Dethlefsen <privat@markus-inger.de>
2016-04-29 17:02:52 +0200

    make config overrideable

--- config/default.js
+++ config/default.js
@@ -1,5 +1,11 @@
-export default {
+const nconf = require('nconf');
+
+const config = {
     'endpoint': {
         'productApi': process.env.NODE_ENV === 'production' ? '/api/product' : '//localhost:3000/api/product'
     }
 }
+
+nconf.defaults(config);
+
+module.exports = nconf.get();
